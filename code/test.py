0008832406cc33022e1e385ee6078142146c6623
Sadegh Karimi <sadegh.krmi@gmail.com>
2023-02-12 20:53:28 +0100

    Update test.py

--- code/test.py
+++ code/test.py
@@ -1,12 +1,5 @@
-import spacy
+from numpy import random
 
-nlp = spacy.blank("en")
-doc = nlp("David Bowie is a PERSON")
+x = random.randint(100)
 
-# Look up the hash for the string label "PERSON"
-person_hash = nlp.vocab.strings["PERSON"]
-print(person_hash)
-
-# Look up the person_hash to get the string
-person_string = nlp.vocab.strings[person_hash]
-print(person_string)
+print(x)
